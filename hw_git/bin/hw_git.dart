8e3888beb32415e5dc6ba37a2c69183b4fa315ce
shaimaa <shaimaathu@MacBook-Air-alkhas-b-Abdullah.local>
2024-02-04 23:19:56 +0300

    shaimaa-althubaiti

---
+++ hw_git/bin/hw_git.dart
@@ -0,0 +1,13 @@
+import 'package:hw_git/hw_git.dart' as hw_git;
+
+void main(List<String> arguments) {
+
+
+    print("numbers from 1 to 10");
+    for(var x = 1 ; x<=10; x++){ //for loop to print the number from 1 to 10
+
+    print(x); // print the number
+
+    }
+
+}
