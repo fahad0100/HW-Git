16145327e11af0837260d4eb5a3383c86d720b5e
AbdulazezAlamri <azoozai533@gmail.com>
2024-02-04 21:04:21 +0300

    azooz

---
+++ hw-git_2/bin/hw_git_2.dart
@@ -0,0 +1,11 @@
+import 'package:hw_git_2/hw_git_2.dart' as hw_git_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hw_git_2.calculate()}!');
+
+
+for(int i = 0; i < 11; i++){
+  print('$i\n');
+}
+
+}
